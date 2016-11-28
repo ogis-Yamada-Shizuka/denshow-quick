4a3c34f1df8cdc6bcf61b9c8f0c9665c7ab96055
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2016-11-28 21:21:47 +0900

    誤って削除したdoc_noのvalidateを戻す

--- app/models/request_detail.rb
+++ app/models/request_detail.rb
@@ -3,10 +3,10 @@ class RequestDetail < ActiveRecord::Base
   belongs_to :doc_type
   belongs_to :chg_type
 
+  validates :doc_no, length: { maximum: 255 }
   validates :sht, length: { maximum: 255 }
   validates :rev, length: { maximum: 255 }
   validates :eo_chgno, length: { maximum: 255 }
-  validates :chg_type, length: { maximum: 255 }
   validates :mcl, length: { maximum: 255 }
   validates :scp_for_smpl, length: { maximum: 255 }
   validates :scml_ln, length: { maximum: 255 }
