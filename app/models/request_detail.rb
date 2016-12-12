195aa67d62147277d0ddc5fb4590c7ed446b41d3
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2016-12-12 19:50:59 +0900

    modify vendor_code validate and upcase letter

--- app/models/request_detail.rb
+++ app/models/request_detail.rb
@@ -4,7 +4,7 @@ class RequestDetail < ActiveRecord::Base
   belongs_to :chg_type
 
   before_validation do
-    %i(doc_no sht rev eo_chgno mcl scp_for_smpl scml_ln).each do |attribute|
+    %i(doc_no sht rev eo_chgno mcl scp_for_smpl scml_ln vendor_code).each do |attribute|
       send(attribute).upcase! if send(attribute).present?
     end
   end
@@ -17,4 +17,7 @@ class RequestDetail < ActiveRecord::Base
   end
 
   validates :doc_type_id, presence: true
+
+  # TODO: rspecセットアップ後にvalidateのテストコードを書くこと
+  validates :vendor_code, length: { maximum: 255 }, format: { without: /[^0-9^A-Z]/ }
 end
