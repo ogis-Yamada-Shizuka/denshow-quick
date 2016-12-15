2fe8e8251373bee486894e6f1859866255ff9250
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2016-12-15 12:18:01 +0900

    不要になったCSV出力用のコントローラ等削除

--- app/controllers/csv/request_application_exports_controller.rb
+++
@@ -1,9 +0,0 @@
-class Csv::RequestApplicationExportsController < ApplicationController
-  def index
-    @q = RequestApplication.ransack(params.fetch(:q, {}).permit(:management_no_cont))
-    @request_applications = @q.result.order(created_at: :DESC)
-  end
-
-  def create
-  end
-end
