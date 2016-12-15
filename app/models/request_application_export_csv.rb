e615017da49d756f5c48e37457448636731f96af
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2016-12-15 15:12:28 +0900

    CSV出力時に重複した機種コードと技術資料番号を削除するように変更

--- app/models/request_application_export_csv.rb
+++ app/models/request_application_export_csv.rb
@@ -2,18 +2,26 @@ require 'csv'
 
 class RequestApplicationExportCsv
   class << self
+    # rubocop:disable all
+    # メソッドチェインするとrubocopエラーが出る(バグ？)ので無効化
     def export(request_applications)
-      csv_data = CSV.generate do |csv|
-        headers = %w(機種コード 技術資料番号)
-        csv << headers
-        request_applications.each do |request_application|
-          request_application.details.each do |detail|
-            values = [request_application.model.code, detail.doc_no]
-            csv << values
-          end
+      CSV.generate { |csv|
+        csv << %w(機種コード 技術資料番号)
+        collect_values(request_applications).each { |value| csv << value }
+      }.encode(Encoding::SJIS)
+    end
+    # rubocop:enable all
+
+    private
+
+    def collect_values(request_applications)
+      values = []
+      request_applications.each do |request_application|
+        request_application.details.each do |detail|
+          values << [request_application.model.code, detail.doc_no]
         end
       end
-      csv_data.encode(Encoding::SJIS)
+      values.uniq
     end
   end
 end
