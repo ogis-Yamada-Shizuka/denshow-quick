be44d372f8c0973b64a37a4cdd0a9af13385b97f
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-01-16 10:32:53 +0900

    add index action to model_code controller

--- app/controllers/for_matching_datas/model_code_controller.rb
+++ app/controllers/for_matching_datas/model_code_controller.rb
@@ -1,5 +1,7 @@
 class ForMatchingDatas::ModelCodeController < ApplicationController
   def index
+    @q = ForMatchingData.ransack(params[:q])
+    @for_matching_datas = params[:q].present? ? @q.result.group(:model_code).order(model_code: :ASC) : []
   end
 
   def destroy
