505db9cf169bd7ea34df34ca1b05eec01c065a68
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-01-17 18:28:31 +0900

    modify matching result view

--- app/controllers/request_applications/matching_controller.rb
+++ app/controllers/request_applications/matching_controller.rb
@@ -5,6 +5,7 @@ class RequestApplications::MatchingController < ApplicationController
   end
 
   def matching_result
+    @request_application = RequestApplication.find(params[:id])
     @matching_results = RequestApplicationMatcher.match(params[:id])
   end
 end
