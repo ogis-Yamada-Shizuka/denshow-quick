5ac6b549e89daa3c91ec5822be3103157f41bb43
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-01-18 19:50:47 +0900

    remove  render_matching_result_table_data method

--- app/helpers/request_applications_helper.rb
+++ app/helpers/request_applications_helper.rb
@@ -1,10 +1,2 @@
 module RequestApplicationsHelper
-  # TODO: 一致・不一致の部分をymlに定義する
-  def render_matching_result_table_data(is_match)
-    if is_match
-      content_tag(:td, '一致')
-    else
-      content_tag(:td, '不一致', style: "color: red;")
-    end
-  end
 end
