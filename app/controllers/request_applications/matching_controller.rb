f996d5bd0062a6144e9c1e9b2f7260ac0e1555be
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-01-19 09:04:26 +0900

    modify matching result action

--- app/controllers/request_applications/matching_controller.rb
+++ app/controllers/request_applications/matching_controller.rb
@@ -6,6 +6,6 @@ class RequestApplications::MatchingController < ApplicationController
 
   def matching_result
     @request_application = RequestApplication.find(params[:id])
-    @matching_results = RequestApplicationMatcher.match(params[:id])
+    @matching_results = @request_application.compare_to_matching_datas
   end
 end
