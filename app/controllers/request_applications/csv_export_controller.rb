ed9e4bdad875fa3378d0a64d50412400f5b84add
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-01-23 14:01:17 +0900

    未選択時にエラーメッセージを表示

--- app/controllers/request_applications/csv_export_controller.rb
+++ app/controllers/request_applications/csv_export_controller.rb
@@ -5,7 +5,11 @@ class RequestApplications::CsvExportController < ApplicationController
   end
 
   def export
-    request_applications = RequestApplication.where(id: params[:request_application][:ids])
-    send_data RequestApplicationExportCsv.export(request_applications), type: 'text/csv; charset=shift_jis', filename: "#{Time.zone.now.to_date}.csv"
+    if params[:request_application].present?
+      request_applications = RequestApplication.where(id: params[:request_application][:ids])
+      send_data RequestApplicationExportCsv.export(request_applications), type: 'text/csv; charset=shift_jis', filename: "#{Time.zone.now.to_date}.csv"
+    else
+      redirect_to search_request_applications_path, alert: t('controller.errors.template.csv_export.not_selected')
+    end
   end
 end
