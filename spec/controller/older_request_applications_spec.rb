6fe9436eebf9c044ac398cd213dcf00293803ea2
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-02-03 14:27:25 +0900

    remove duplicate describe

--- spec/controller/older_request_applications_spec.rb
+++ spec/controller/older_request_applications_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe OlderRequestApplications::RequestApplicationsController, type: :controller do
-
-  describe 'OlderRequestApplicationsController test' do
-    describe 'GET #index' do
-      before { get :index }
-      it { expect(response.status).to eq(200) }
-    end
+  describe 'GET #index' do
+    before { get :index }
+    it { expect(response.status).to eq(200) }
   end
 end
