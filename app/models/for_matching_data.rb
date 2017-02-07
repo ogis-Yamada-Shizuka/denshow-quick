62e6662b4684921d50862f44355a7ebc387aba0e
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-02-07 20:00:28 +0900

    比較用の値取得メソッドを各modelに追い出し

--- app/models/for_matching_data.rb
+++ app/models/for_matching_data.rb
@@ -5,4 +5,18 @@ class ForMatchingData < ActiveRecord::Base
   end
 
   validates :document_no, uniqueness: true
+
+  def compare_attributes
+    {
+      doc_no: doc_no,
+      doc_type: doc_type_str,
+      sht: sht,
+      rev: revision,
+      eo_chgno: eo_chgno,
+      chg_type: chg_type_str,
+      mcl: mcl,
+      scp_for_smpl: scp_for_smpl,
+      scml_ln: scml
+    }.map { |key, value| [key, value.to_s] }.to_h
+  end
 end
--- app/models/request_detail.rb
+++ app/models/request_detail.rb
@@ -21,4 +21,18 @@ class RequestDetail < ActiveRecord::Base
   validates :doc_no, presence: true
   validates :sht, presence: true
   validates :rev, presence: true
+
+  def compare_attributes
+    {
+      doc_no: doc_no,
+      doc_type: doc_type.name,
+      sht: sht,
+      rev: rev,
+      eo_chgno: eo_chgno,
+      chg_type: chg_type&.name,
+      mcl: mcl,
+      scp_for_smpl: scp_for_smpl,
+      scml_ln: scml_ln
+    }.map { |key, value| [key, value.to_s] }.to_h
+  end
 end
