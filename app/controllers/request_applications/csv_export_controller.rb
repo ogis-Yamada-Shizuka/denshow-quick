fa6d17e40a5adf7dd99282de966fb4a41f59451b
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-02-14 15:29:19 +0900

    I画面(K-PEACEデータ取得用CSV)のCSV出力時のファイル名を変更

--- app/controllers/request_applications/csv_export_controller.rb
+++ app/controllers/request_applications/csv_export_controller.rb
@@ -7,9 +7,16 @@ class RequestApplications::CsvExportController < ApplicationController
   def export
     if params[:request_application].present?
       request_applications = RequestApplication.where(id: params[:request_application][:ids])
-      send_data RequestApplicationExportCsv.export(request_applications), type: 'text/csv; charset=shift_jis', filename: "#{Time.zone.now.to_date}.csv"
+      send_data RequestApplicationExportCsv.export(request_applications), type: 'text/csv; charset=shift_jis', filename: filename
     else
       redirect_to search_request_applications_path, alert: t('message.template.csv_export.management_no_is_not_selected')
     end
+  end
+
+  private
+
+  def filename
+    now = Time.zone.now
+    "#{t('file_name.export_csv_for_k_peace')}_#{now.strftime('%Y%m%d')}_#{now.strftime('%H%M')}.csv"
   end
 end
