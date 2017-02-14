57f13105833e61553303e485c9497c297c9c38c1
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-02-14 15:19:39 +0900

    L画面(リコンサイル結果)のCSV出力時のファイル名を変更

--- app/controllers/request_applications/matching_controller.rb
+++ app/controllers/request_applications/matching_controller.rb
@@ -11,8 +11,15 @@ class RequestApplications::MatchingController < ApplicationController
         @matching_results = @request_application.compare_to_matching_datas
       end
       format.csv do
-        send_data @request_application.export_matching_result_csv, type: 'text/csv; charset=shift_jis', filename: "#{t('file_name.matching_result_export_csv')}_#{Time.zone.today}.csv"
+        send_data @request_application.export_matching_result_csv, type: 'text/csv; charset=shift_jis', filename: filename
       end
     end
+  end
+
+  private
+
+  def filename
+    now = Time.zone.now
+    "#{t('file_name.matching_result_export_csv')}_#{now.strftime('%Y%m%d')}_#{now.strftime('%H%M')}.csv"
   end
 end
