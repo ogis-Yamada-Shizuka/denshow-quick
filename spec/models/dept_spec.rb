4b01b45ad89b637ed9ad112365e6ad49606bbf7d
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-02-22 18:14:02 +0900

    add dept model rspec

---
+++ spec/models/dept_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'csv'
+
+RSpec.describe Dept, type: :model do
+  describe 'Attribute definition' do
+    it { is_expected.to respond_to(:name) }
+    it { is_expected.to respond_to(:project) }
+  end
+
+  describe 'validates' do
+    subject { dept }
+    let(:dept) { create(:dept) }
+    it { is_expected.to be_valid }
+
+    describe 'name' do
+      context '値がnilの場合' do
+        subject { dept.valid? }
+        before { dept[:name] = nil }
+
+        it { is_expected.to be_falsey }
+      end
+
+      context '値がユニークではない場合' do
+        subject { same_attribute_dept.valid? }
+        let(:same_attribute_dept) { build(:dept, name: dept.name) }
+
+        it { is_expected.to be_falsey }
+      end
+    end
+  end
+end
