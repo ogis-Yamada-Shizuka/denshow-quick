28da5b5fcc9041902ed5dfed1cf3890a39539d82
i-yamaguchi <i.yamaguchi.froide@gmail.com>
2017-02-22 18:23:28 +0900

    add project model rspec

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  describe 'Attribute definition' do
+    it { is_expected.to respond_to(:name) }
+  end
+
+  describe 'validates' do
+    subject { project }
+    let(:project) { create(:project) }
+    it { is_expected.to be_valid }
+
+    describe 'name' do
+      context '256文字以上の場合' do
+        it do
+          project[:name] = 'a' * 256
+          is_expected.to be_invalid
+        end
+      end
+    end
+  end
+end
